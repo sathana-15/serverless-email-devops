24a0be3b746a18fcad88cecf5fa32b4a43ace2a0
sathana <sathana.rd2023cse@sece.ac.in>
2025-10-15 19:56:31 +0530

    Setup Terraform to auto-zip Lambda; remove old lambda.zip

---
+++ main.tf
@@ -0,0 +1,85 @@
+# ---------------------------
+# IAM Role for Lambda
+# ---------------------------
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-ses-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Effect    = "Allow",
+      Sid       = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# ---------------------------
+# Automatically zip Lambda folder
+# ---------------------------
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "../lambda"
+  output_path = "../terraform/lambda.zip"
+}
+
+# ---------------------------
+# Lambda function
+# ---------------------------
+resource "aws_lambda_function" "email_lambda" {
+  filename      = data.archive_file.lambda_zip.output_path
+  function_name = var.lambda_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "handler.lambda_handler"
+  runtime       = "python3.11"
+
+  environment {
+    variables = {
+      AWS_REGION       = var.aws_region
+      SES_SENDER_EMAIL = var.ses_sender_email
+    }
+  }
+}
+
+# ---------------------------
+# API Gateway HTTP API
+# ---------------------------
+resource "aws_apigatewayv2_api" "api" {
+  name          = "email-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.email_lambda.arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /send"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# ---------------------------
+# Lambda permission for API Gateway
+# ---------------------------
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.email_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
